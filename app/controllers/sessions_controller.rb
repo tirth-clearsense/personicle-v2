799a1c499c7ebaf6368ecc5da05786d9fb36866e
tirth-clearsense <tpatel@clearsense.com>
2022-03-11 11:07:07 -0600

    logout redirect fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   
     def destroy
       session[:oktastate] = nil
+      flash[:success] = "You have logged out"
       redirect_to root_path
       # redirect_to ENV['OKTA_URL'] + "/login/signout?fromURI=" + request.base_url + "/logout"
     end
